7c969b65efb33778b5d5fa25c188b4a8a8c308ef
merseyviking <mersey.viking@gmail.com>
2014-07-09 16:42:44 +0100

    Awesome sauce.

---
+++ main.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2014 GeoSpark
+#
+# Released under the MIT License (MIT)
+# See the LICENSE file, or visit http://opensource.org/licenses/MIT
+
+if __name__ == '__main__':
+    print('Hello world!')
